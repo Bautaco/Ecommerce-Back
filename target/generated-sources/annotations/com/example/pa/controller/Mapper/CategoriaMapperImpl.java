041d5c912ce344574e6600f03bccc14d6edc79c4
matipiermarini <matiaspiermarini45@gmail.com>
2024-12-02 23:05:57 -0300

    promociones terminado con metodo get post delete y listar activas y dentro del rango de fecha actual

--- target/generated-sources/annotations/com/example/pa/controller/Mapper/CategoriaMapperImpl.java
+++ target/generated-sources/annotations/com/example/pa/controller/Mapper/CategoriaMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-01T16:57:50-0300",
+    date = "2024-12-02T20:38:48-0300",
     comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.40.0.z20241112-1021, environment: Java 17.0.13 (Eclipse Adoptium)"
 )
 @Component
